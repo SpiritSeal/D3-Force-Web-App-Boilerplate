936bad58e9d8ce7ff2ef3a975f3467ea520a537d
Saketh Reddy <45369057+SpiritSeal@users.noreply.github.com>
2022-05-24 12:11:58 -0700

    broke it

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Boilerplate by Saketh YS Reddy
 import * as d3 from "https://cdn.skypack.dev/d3@7";
 
-var colorScale = ['orange', 'lightblue', '#B19CD9'];
-
 var width = window.innerWidth;
 var height = window.innerHeight; 
 
+
+//Some example relational data
 const graph = ({
-    nodes: Array.from({length:13}, () => ({})),
+    nodes: Array.from({length:13}, (i) => ({index: i})),
     links: [
       { source: 0, target: 1 },
       { source: 1, target: 2 },
@@ -31,7 +31,8 @@ const graph = ({
   })
 
 
-// Create the SVG, links, and nodes
+
+// Append a g element to the SVG, create links and nodes
 const svg = d3.select("svg")
 const g = svg.append("g")
   .attr("transform", "translate("+width/2+","+height/2+")"),
@@ -52,23 +53,14 @@ node = g
 svg .call(d3.zoom().on("zoom", zoomed));
 
 function zoomed({transform}) {
-  console.log(transform);
+  // console.log(transform);
   g.attr("transform", transform);
 }
 const zoom = d3.zoom()
       .scaleExtent([1, 40])
       .on("zoom", zoomed);
-      
-svg.call(zoom.transform, d3.zoomIdentity.translate(width/2, height/2));
 
-//Inititalize 0,0 at the center of the screen
-// zoomed({
-//   "k": 1,
-//   "x": 100,
-//   "y": 100
-//   // "x": width/2,
-//   // "y": height/2
-// })
+svg.call(zoom.transform, d3.zoomIdentity.translate(width/2, height/2));
 
 const simulation = d3
   .forceSimulation()
@@ -77,14 +69,6 @@ const simulation = d3
   .force("center", d3.forceCenter())
   .force("link", d3.forceLink(graph.links))
   .on("tick", tick);
-
-const dragHandle = d3
-  .drag()
-  .on("start", dragstart)
-  .on("drag", dragged)
-  .on("end", dragended);
-
-node.call(dragHandle).on("click", click);
 
 function tick() {
   link
@@ -97,11 +81,14 @@ function tick() {
     .attr("cy", d => d.y);
 }
 
-function click(event, d) {
-  //Change what happens when nodes are clicked here
-  console.log("clicked", this);
-  simulation.alpha(1).restart();
-}
+// Drag and click functionality
+const dragHandle = d3
+  .drag()
+  .on("start", dragstart)
+  .on("drag", dragged)
+  .on("end", dragended);
+
+node.call(dragHandle).on("click", click);
 
 function dragstart(event, d) {
   //Change what happens when they are initially held down here
@@ -118,9 +105,62 @@ function dragged(event, d) {
 }
 
 function dragended(event, d) {
-  //Change what happens while they stop moving here
+  //Change what happens while they are released here
   if (!event.active) simulation.alphaTarget(0);
   d.fx = null;
   d.fy = null;
   d3.select(this).classed("fixed", false);
+}
+
+// function spawn(parent) {
+//   console.log("spawning from parent", parent.index);
+//   console.log(parent);
+//   parent.index = graph.nodes.length;
+//   // parent.vx++;
+//   // parent.vy++;
+//   graph.nodes.push(parent);
+//   console.log(graph.nodes);
+//   simulation.alpha(1).restart();
+// }
+
+function click(event, d) {
+  //Change what happens when nodes are clicked here
+  console.log("clicked", this);
+  spawn(d);
+  // simulation.alpha(1).restart();
+}
+
+//----------------------------------------------------------------------------------
+//Create Node
+function spawn(source) {
+  graph.nodes.push(source);
+
+  for (const target of nodes) {
+    if (inrange(source, target)) {
+      graph.links.push({source, target});
+    }
+  }
+
+  link = link
+    .data(graph.links)
+    .join("line");
+
+  node = node
+    .data(graph.nodes)
+    .join(
+      enter => enter.append("circle").attr("r", 0)
+        .call(enter => enter.transition().attr("r", 5))
+        .call(dragHandle).on("click", click),
+      update => update,
+      exit => exit.remove()
+    );
+
+  simulation.nodes(graph.nodes);
+  simulation.force("link").links(graph.links);
+  simulation.alpha(1).restart();
+
+  svg.property("value", {
+    nodes: graph.nodes.map(d => ({id: d.index})),
+    links: graph.links.map(d => ({source: d.source.index, target: d.target.index}))
+  });
 }
